68617f999821359d15ce6fd8c6a28356b2ad4d89
824559HUjunfei <junfeihu375@gmail.com>
2020-04-07 15:28:14 +0800

    添加Order实体类

---
+++ src/main/java/cn/itcast/day01/Pojo/Order.java
@@ -0,0 +1,40 @@
+package cn.itcast.day01.Pojo;
+
+public class Order {
+    private Integer id;
+    private Integer userId;
+    private String orderNumber;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", orderNumber='" + orderNumber + '\'' +
+                '}';
+    }
+}
